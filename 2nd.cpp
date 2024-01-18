40c82702f0afe7360d6249501be05b8309c37614
PRABODH1243 <prabodhbadimi1@gmail.com>
2024-01-18 23:32:44 +0530

    hello

---
+++ 2nd.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+struct SubMarks {
+    string sub;
+    int marks;
+
+    void getSubMarks(int marks, string sub) {
+        this->sub = sub;
+        this->marks = marks;
+    }
+    void displaySubMarks() {
+        cout << "Subject: " << sub << endl;
+        cout << "Marks: " << marks << endl;
+    }
+    void updateMarks(int *marks) {
+        cout << "Enter the updated marks: ";
+        cin >> *marks;
+    }
+};
+
+struct StudentMarks {
+    struct SubMarks regularMarks[5];
+
+    void getStudentMarks() {
+        for (int i = 0; i < 5; i++) {
+            cout << "Enter the " << i + 1 << "th subject details" << endl;
+            string sub;
+            int marks;
+            cout << "Enter the subject: ";
+            cin >> sub;
+            cout << "Enter the marks: ";
+            cin >> marks;
+            regularMarks[i].getSubMarks(marks, sub);
+        }
+    }
+    void getFailedSubjects() {
+        cout << "Failed Subjects:" << endl;
+        for (int i = 0; i < 5; i++) {
+            if (regularMarks[i].marks < 35) {
+                regularMarks[i].displaySubMarks();
+            }
+        }
+    }
+    void updateFailedSubjects() {
+        cout << "Updating Failed Subjects:" << endl;
+        for (int i = 0; i < 5; i++) {
+            if (regularMarks[i].marks < 35) {
+                regularMarks[i].updateMarks(&regularMarks[i].marks);
+            }
+        }
+    }
+};
+
+int main() {
+    StudentMarks student;
+
+    cout << "Enter the student details:" << endl;
+    student.getStudentMarks();
+
+    student.getFailedSubjects();
+
+    student.updateFailedSubjects();
+
+    cout << "Updated Marks:" << endl;
+    student.getFailedSubjects();
+
+    return 0;
+}
---
+++ 3rd.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Student {
+    int rollno;
+    string name;
+    int date_of_birth;
+};
+
+int main() {
+    int n = 0;
+    cout << "Enter the number of students : ";
+    cin >> n;
+    Student dents[n];
+    for (int i = 0; i<n; i++ ) {
+        cout << "Enter " << i+1 <<"th student Name: ";
+        cin >> dents[i].name;
+        cout << "Enter " << i <<"th student Roll Number: ";
+        cin >> dents[i].rollno;
+        cout << "Enter " << i <<"th student dateofbirth: ";
+        cin >> dents[i].date_of_birth;
+    }
+    for (int i = 0; i < n; i++) {
+        cout << "Detailes of " << i+1 << "th student: " << endl;
+        cout << "student Name: " << dents[i].name << endl;
+        cout << "student Roll Number: " << dents[i].rollno << endl;
+        cout << "student dob: " << dents[i].date_of_birth << endl;
+    }
+    return 0;
+}
