1edc6454ea63ffbf04701d167c2575459615afc3
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-01-21 23:16:15 +0530

    Add files via upload

---
+++ 4th.cpp
@@ -0,0 +1,20 @@
+// write a cpp program to read, display n numbers (take n from user) and find mean of n numbers using an array
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 0, mean = 0;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        mean += arr[i];
+    }
+    mean /= n;
+    cout << "array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << mean <<endl;
+}
---
+++ 5th.sum.cpp
@@ -0,0 +1,58 @@
+// // wap in cpp to find largest and 2nd largest num from n nums using an array ?
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int n;
+//     cout << "Enter the number of elements: ";
+//     cin >> n;
+//     if (n < 2) {
+//         cout << "finding the largest and second-largest." << endl;
+//         return 1;
+//     }
+//     int nums[n];
+//     cout << "Enter the elements: ";
+//     for (int i = 0; i < n; i++) {
+//         cin >> nums[i];
+//     }
+//     int largest = min ;          
+//     int secondLargest = min;    
+//     for (int i = 0; i < n; i++) {
+//         if (nums[i] > largest) {
+//             secondLargest = largest;
+//             largest = nums[i];
+//         } else if (nums[i] > secondLargest && nums[i] != largest) {
+//             secondLargest = nums[i];
+//         }
+//     }
+//     cout << "Largest Number: " << largest << endl;
+//     cout << "Second Largest Number: " << (secondLargest != min ? secondLargest : -1) << endl;
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+
+void largestNumber(int *arr, int size) {
+    int maximum1=arr[0], maximum2=arr[0];
+    for (int i = 0; i < size; i++) {
+        if (arr[i] > maximum1) {
+            maximum2 = maximum1;
+            maximum1 = arr[i];
+        }
+    }
+    cout << maximum1 << " " << maximum2 << endl;
+    return;
+}
+int main() {
+    int n = 0;
+    cout << "Enter the length of the array: ";
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    largestNumber(arr,n);
+
+    return 0;
+}
