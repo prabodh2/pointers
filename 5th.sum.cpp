5a083f0f617edb36949da42bbfef8b135dadf8bf
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-01-29 20:09:41 +0530

    Update 5th.sum.cpp

--- 5th.sum.cpp
+++ 5th.sum.cpp
@@ -28,10 +28,8 @@
 //     cout << "Second Largest Number: " << (secondLargest != min ? secondLargest : -1) << endl;
 //     return 0;
 // }
-
 #include <iostream>
 using namespace std;
-
 void largestNumber(int *arr, int size) {
     int maximum1=arr[0], maximum2=arr[0];
     for (int i = 0; i < size; i++) {
@@ -51,7 +49,6 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-
     largestNumber(arr,n);
 
     return 0;
