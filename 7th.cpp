2e8b2ee2361408d335279387974ad967b28e53a7
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-01-29 20:08:36 +0530

    Update 7th.cpp

--- 7th.cpp
+++ 7th.cpp
@@ -1,6 +1,5 @@
 // #include <iostream>
 // using namespace std;
-
 // void insertBeginning(int* array, int element, int &n) {
 //     for (int i = n-1; i >= 0; i--) {
 //         array[i+1] = array[i];
