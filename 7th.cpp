85bd1b8cc75fd4b25c8084e9a4f98f9e2253747f
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-02-12 22:59:50 +0530

    Update 7th.cpp

--- 7th.cpp
+++ 7th.cpp
@@ -1,37 +1,37 @@
 
 // // //     //Traverse 
 
-// // #include <iostream>
-// // using namespace std;
+//  #include <iostream>
+//  using namespace std;
 
-// // void insertBeginning(int* array, int element, int &n) {
-// //     for (int i = n-1; i >= 0; i--) {
-// //         array[i+1] = array[i];
-// //     }
-// //     array[0] = element;
-// //     n++;
-// // }
-// // void displayArr(int *array, int n) {
-// //     cout << "Array : [ ";
-// //     for ( int i = 0; i < n; i++) {
-// //         if (i == n - 1) {
-// //             cout << array[i] << " ]" << endl;
-// //         } else {
-// //             cout << array[i] << ", " ;
-// //         }
-// //     }
-// // }
-// // void InsertAtBeginning() {
-// //     int numOfEleInArr; 
-// //     cout << "Enter length of array: ";
-// //     cin >> numOfEleInArr;
-// //     int arr[10] = {0}; 
-// //     for (int i = 0; i < numOfEleInArr; i++) {
-// //         cin >> arr[i];
-// //     }
-// //     cout << "current ";
-// //     displayArr(arr, numOfEleInArr);
-// //     int newEle = 0;
+//  void insertBeginning(int* array, int element, int &n) {
+//      for (int i = n-1; i >= 0; i--) {
+//          array[i+1] = array[i];
+//     }
+//      array[0] = element;
+//      n++;
+//  }
+//  void displayArr(int *array, int n) {
+//     cout << "Array : [ ";
+//      for ( int i = 0; i < n; i++) {
+//          if (i == n - 1) {
+//              cout << array[i] << " ]" << endl;
+//          } else {
+//              cout << array[i] << ", " ;
+//          }
+//      }
+//  }
+//  void InsertAtBeginning() {
+//      int numOfEleInArr; 
+//      cout << "Enter length of array: ";
+//      cin >> numOfEleInArr;
+//      int arr[10] = {0}; 
+//      for (int i = 0; i < numOfEleInArr; i++) {
+//          cin >> arr[i];
+//      }
+//      cout << "current ";
+//      displayArr(arr, numOfEleInArr);
+//      int newEle = 0;
 // //     cout << "Enter the element to append: ";
 // //     cin >> newEle;
 // //     insertBeginning(arr,newEle, numOfEleInArr);
