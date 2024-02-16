eda40c3887232714f0f42ad9a866c18ff3e4e21c
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-02-16 22:28:12 +0530

    Update Allinone.cpp

--- Allinone.cpp
+++ Allinone.cpp
@@ -6,7 +6,6 @@ void insertAtEnd(int* arr, int ele, int& n) {
     arr[n] = ele;
     n++;
 }
-
 void insertAtBeginning(int* arr, int ele, int& n) {
     for (int i = n; i > 0; i--) {
         arr[i] = arr[i-1];
@@ -25,7 +24,6 @@ void insertAtPosition(int* arr, int ele, int pos, int& n) {
     arr[pos] = ele;
     n++;
 }
-
 void displayArr(int *arr, int n) {
     cout << "Array : [ ";
     for (int i = 0; i < n; i++) {
