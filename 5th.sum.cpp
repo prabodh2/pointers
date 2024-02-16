9b09e806973ec56f98655929243ec0c6c1b28058
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-02-16 22:20:58 +0530

    Update 5th.sum.cpp

--- 5th.sum.cpp
+++ 5th.sum.cpp
@@ -1,4 +1,4 @@
-// // wap in cpp to find largest and 2nd largest num from n nums using an array ?
+ // wap in cpp to find largest and 2nd largest num from n nums using an array ?
 // #include <iostream>
 // using namespace std;
 // int main() {
@@ -52,6 +52,5 @@ int main() {
     }
 
     largestNumber(arr,n);
-
     return 0;
 }
